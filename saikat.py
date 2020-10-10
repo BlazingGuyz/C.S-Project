f0653e78f2dfc0ebbe319d9d8b498c94ff9154b4
TOXIQxEAGLE <sekhardhar14@gmail.com>
2020-10-10 21:10:36 +0530

    python

--- saikat.py
+++ saikat.py
@@ -16,13 +16,13 @@ while True:
     k = cv2.waitKey(1)
     if k%256 == 27:
         # ESC pressed
-        print("Escape hit, closing...")
+        print("Photo Saved, closing...")
         break
     elif k%256 == 32:
         # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
+        img_name = "GUEST_{}.png".format(img_counter)
         cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
+        print("{} Captured!".format(img_name))
         img_counter += 1
 
 cam.release()
