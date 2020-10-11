2bad0e5ca8bb75002e7e0bb6fd2f2beaa55da554
TOXIQxEAGLE <sekhardhar14@gmail.com>
2020-10-11 17:23:14 +0530

    python

--- eye.py
+++ eye.py
@@ -1,39 +1,28 @@
-import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier('C:\\Users\\HP\\Desktop\\C.S-Project\\haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('C:\\Users\\HP\\Desktop\\C.S-Project\\haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-vid = cv2.VideoCapture(0) 
-  
-while(True): 
-      
-    # Capture the video frame 
-    # by frame 
-    ret, frame = vid.read() 
-  
-  
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+video = cv2.VideoCapture(0) 
+# 8. Variable
+a = 0
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-     roi_gray = gray[y:y+h, x:x+w]
-     roi_color = img[y:y+h, x:x+w]
-     eyes = eye_cascade.detectMultiScale(roi_gray)
-     for (ex,ey,ew,eh) in eyes:
-         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+while video.isOpened():
+    _, video = video.read()
+    gray = cv2.cvtColor(video, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-  # Display the resulting frame 
-    cv2.imshow('frame', frame) 
-      
-    # the 'q' button is set as the 
-    # quitting button you may use any 
-    # desired button of your choice 
-    if cv2.waitKey(1) & 0xFF == ord('q'): 
+    for (x, y , w ,h) in faces:
+        cv2.rectangle(video, (x,y), (x+w, y+h), (255, 0 , 0), 3)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = video[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey ,ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0, 255, 0), 5)
+
+    # Display the output
+    cv2.imshow('video', video)
+    if cv2.waitKey() & 0xFF == ord('q'):
         break
-  
-# After the loop release the cap object 
-vid.release() 
-# Destroy all the windows 
-cv2.destroyAllWindows() 
+
+cap.release()
