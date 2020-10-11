3325ff0eb6bbaf94d630d951c5f4d9ccd5aeda72
TOXIQxEAGLE <sekhardhar14@gmail.com>
2020-10-11 16:44:32 +0530

    python

--- eye.py
+++ eye.py
@@ -1,10 +1,18 @@
 import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier('/root/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('/root/opencv/data/haarcascades/haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('C:\\Users\\HP\\Desktop\\C.S-Project\\haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('C:\\Users\\HP\\Desktop\\C.S-Project\\haarcascade_eye.xml')
 
-img = cv2.imread('/root/Desktop/baby.jpg')
+vid = cv2.VideoCapture(0) 
+  
+while(True): 
+      
+    # Capture the video frame 
+    # by frame 
+    ret, frame = vid.read() 
+  
+  
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -15,6 +23,17 @@ for (x,y,w,h) in faces:
      eyes = eye_cascade.detectMultiScale(roi_gray)
      for (ex,ey,ew,eh) in eyes:
          cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+  # Display the resulting frame 
+    cv2.imshow('frame', frame) 
+      
+    # the 'q' button is set as the 
+    # quitting button you may use any 
+    # desired button of your choice 
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
+  
+# After the loop release the cap object 
+vid.release() 
+# Destroy all the windows 
+cv2.destroyAllWindows() 
