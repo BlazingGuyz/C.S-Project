b289937bd971885ac8b6ff5f2cb5186bf41c1010
TOXIQxEAGLE <sekhardhar14@gmail.com>
2020-10-11 16:25:16 +0530

    python

--- saikat.py
+++ saikat.py
@@ -1,30 +1,36 @@
-import cv2
+import cv2, time
+import random
 
-cam = cv2.VideoCapture(0)
-
-cv2.namedWindow("test")
-
-img_counter = 0
+# 1.creating a video object
+video = cv2.VideoCapture(0) 
+# 8. Variable
+a = 0
 
 while True:
-    ret, frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
+    a = a + 1
+    #print(a)
+    # 3.Create a frame object
+    check, frame = video.read()
+    #print(check)
+    #print(frame)
+    
+    # 6. Converting to grayscale
+    #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    # 4.show the frame!
+    cv2.imshow("Capturing",frame)
+    
+    # 5. for press any key to out (miliseconds)
+    #cv2.waitKey(0)
+    
+    # 7. for playing 
+    key = cv2.waitKey(1)
+    
+    if key == ord('q'):
         break
-    cv2.imshow("test", frame)
+showPic = cv2.imwrite(str(random.randint(1100,111110)) + ".jpg", frame)
+print(a)
+# 2. shutdown the camera
+video.release()
 
-    k = cv2.waitKey(1)
-    if k%256 == 27:
-        # ESC pressed
-        print("Photo Saved, closing...")
-        break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "GUEST_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} Captured!".format(img_name))
-        img_counter += 1
-
-cam.release()
-
-cv2.destroyAllWindows()
+cv2.destroyAllWindows 
+ 
